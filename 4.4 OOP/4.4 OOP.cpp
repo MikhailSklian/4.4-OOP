e6c74978ff3cf0b18b7071196183b040656053e6
MikhailSklian <147310394+MikhailSklian@users.noreply.github.com>
2024-05-16 18:07:32 +0300

    Update 4.4 OOP.cpp

--- 4.4 OOP/4.4 OOP.cpp
+++ 4.4 OOP/4.4 OOP.cpp
@@ -1,4 +1,6 @@
-﻿#include "MyVector.h"
+//X (N) и Y (N) - исходные векторы. Получить вектор Z с элементами
+//z i= a*x i- b*y i, i = 0, n-1, a, b – минимальные значения векторов X, Y.
+#include "MyVector.h"
 #include <iostream>
 #include <math.h>
 using namespace std;
